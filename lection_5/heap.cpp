7a3f0ca0019fb184822beeffb979b5b0cf7e1d4c
ads2025-turgenbaev <m.turgenbayev@kbtu.kz>
2025-10-02 07:59:03 +0500

    lection 5
    heap, reverse LL

---
+++ lection_5/heap.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+using namespace std;
+
+class Heap {
+private:
+    vector<int> heap;
+
+    int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    void swap(int index1, int index2) {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+public:
+    void insert(int value) {
+        heap.push_back(value);
+
+        int current = heap.size() - 1;
+
+        while (current > 0) {
+            if (heap[current] > heap[parent(current)]) {
+                swap(current, parent(current));
+            }
+
+            current = parent(current);
+        }
+    }
+
+    int remove() {
+        if (heap.empty()) {
+            return INT_MIN;
+        }
+
+        int maxValue = heap.front();
+
+        if (heap.size() == 1) {
+            heap.pop_back();
+        }
+        else {
+            heap[0] = heap.back();
+            heap.pop_back();
+            heapify(0);
+        }
+
+        return maxValue;
+    }
+
+    void printHeap() {
+        cout << "\n[";
+        for (int i = 0; i < heap.size(); i++) {
+            cout << heap[i];
+            if (i < heap.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+    }
+
+    void heapify(int index) {
+        int maxIndex = index;
+
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap[leftIndex] > heap[maxIndex]) {
+                maxIndex = leftIndex;
+            }
+
+            if (rightIndex < heap.size() && heap[rightIndex] > heap[maxIndex]) {
+                maxIndex = rightIndex;
+            }
+
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            }
+            else {
+                return;
+            }
+        }
+    }
+};
+
+int main() {
+
+    Heap* myHeap = new Heap();
+
+    myHeap->insert(95);
+    myHeap->insert(75);
+    myHeap->insert(80);
+    myHeap->insert(55);
+    myHeap->insert(60);
+    myHeap->insert(50);
+    myHeap->insert(65);
+
+
+    myHeap->printHeap();
+
+    myHeap->remove();
+
+    myHeap->printHeap();
+
+    myHeap->remove();
+
+    myHeap->printHeap();
+
+
+
+    return 0;
+}
